73ec953439f44a98aa78665181b413f68a5a0fc5
Chengxin.Cai <chengxin.cai@gmail.com>
2016-05-10 13:21:24 +0800

    add python utils

---
+++ PythonUtils/Common.py
@@ -0,0 +1,226 @@
+"""
+   provide some common utilities
+
+"""
+
+# @@@ START COPYRIGHT @@@
+#
+# (C) Copyright 2013-2015 Hewlett-Packard Development Company, L.P.
+#
+# @@@ END COPYRIGHT @@@
+
+
+__all__ = ( 'check_module',
+            'run_cmd',
+            'make_enum',
+            'encode_int',
+            'decode_int',
+            'time_it',
+            'which',
+            'get_hostid')
+
+import os
+from subprocess import Popen, PIPE
+import threading
+from datetime import datetime
+import base64
+import struct
+
+
+#constants
+SUCCESS = 0
+FAILURE = 1
+
+E_TIMEOUT = -124
+E_NONEXE  = -126
+E_NONEXT  = -127
+
+
+def check_module(module, version=None):
+    """ check whether a module is installed,
+       (optional) and with the required version
+    """
+    try:
+        m = __import__(module)
+        if version is not None:
+            try:
+                v = getattr(m, 'version', getattr(m, '__version__'))
+                if v < str(version):
+                    return False
+                else:
+                    return True
+
+            except AttributeError:
+                return False
+        else:
+            return True
+
+    except ImportError:
+        return False
+
+
+class Command(object):
+    """
+       a class to implement running a command with timeout
+    """
+    cmdstr = None
+    process = None
+    status = None
+    output, error = '', ''
+
+    def __init__(self, cmdstr):
+        self.cmdstr = cmdstr
+
+    def run(self, timeout=None, test=False):
+        """ Run a command then return: (status, output + error). """
+        def target():
+            """ a closure which run the command"""
+            try:
+                if __debug__ or test:
+                    print self.cmdstr
+
+                if test:
+                    self.status, self.output = 0, ''
+                else:
+                    self.process = Popen(self.cmdstr, shell=True,
+                                                  stdout=PIPE, stderr=PIPE)
+                    self.output, self.error = self.process.communicate()
+                    self.status = self.process.returncode
+            except Exception:
+                self.status = -1
+                self.error = 'Unknown error'
+
+        thread = threading.Thread(target=target)
+        thread.start()
+        thread.join(timeout)
+
+        if thread.is_alive():
+            self.process.terminate()
+            thread.join()
+            self.status = E_TIMEOUT
+            self.error = "TIMEOUT"
+
+        return self.status, self.output
+
+
+def run_cmd(cmdstr, timeout=None, test=False):
+    """
+       a wrapper to call Command.run, it performs some checks before invoke
+       the command
+    """
+    cmd = cmdstr.split(' ')[0]
+    if cmd.startswith('/'):
+        if not os.path.exists(cmd):
+            return E_NONEXT, '{0}: NO SUCH FILE OR DIRECTORY'.format(cmd)
+
+        elif not os.access(cmd, os.X_OK):
+            return E_NONEXE, '{0}: PERMISSION DENIED'.format(cmd)
+
+    command = Command(cmdstr)
+    status, output = command.run(timeout, test)
+
+    return status, output
+
+
+def make_enum(enum_type='Enum', base_classes=None, methods=None, **attrs):
+    """
+    Generates a enumeration with the given attributes.
+    """
+    # Enumerations can not be initalized as a new instance
+    def __init__(_instance, *_args, **_kws):
+        raise RuntimeError('%s types can not be initialized.' % enum_type)
+
+    if base_classes is None:
+        base_classes = ()
+
+    if methods is None:
+        methods = {}
+
+    base_classes = base_classes + (object,)
+    for key, val in methods.iteritems():
+        methods[key] = classmethod(val)
+
+    attrs['enums'] = attrs.copy()
+    methods.update(attrs)
+    methods['__init__'] = __init__
+    return type(enum_type, base_classes, methods)
+
+
+
+def encode_int(n):
+    """
+       encode an integer to a string using base64
+    """
+    data = struct.pack('i', n)
+    return base64.b64encode(data)
+
+
+def decode_int(s):
+    """
+       decode a string to an integer using base64
+    """
+    data = base64.b64decode(s)
+    return struct.unpack('i', data)[0]
+
+
+def time_it(title_, sub, *args, **kws):
+    """Calling a function and show the start time, end time
+       and elapsed time for running it"""
+
+    def format_time (time_):
+        """format a time value"""
+        return time_.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    start_t = datetime.now()
+
+    ret = sub(*args, **kws)
+
+    end_t = datetime.now()
+
+    delta = end_t - start_t
+
+    if __debug__:
+        print '{0} start at {1}, end at {2}'.format(
+               title_, format_time(start_t), format_time(end_t))
+
+        print '{0} elapsed: {1:.4f} seconds'.format(
+               title_, delta.seconds + 1e-6 * delta.microseconds)
+
+    return (delta, ret)
+
+
+def which(cmd):
+    """ simulate the which shell command """
+    for path in os.getenv('PATH').split(":"):
+        full_path = os.path.join(path, cmd)
+        if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+
+    return None
+
+
+def get_hostid(buf=[]):
+    """ get the host id by running /usr/bin/hostid, and buffer it """
+    if len(buf) == 0:
+        retval, out = run_cmd('/usr/bin/hostid')
+        if retval == SUCCESS:
+            buf.append(int(out.split('\n')[0], 16))
+        else:
+           buf.append(0)
+
+    return buf[0]
+
+
+def get_input(prompt, hint=None, default=None, convert=None):
+    if hint:
+        prompt += " (" + hint
+        if default:
+            prompt += ", [%s]" % default
+
+        prompt += ")"
+
+    answer = raw_input(prompt + ": ")
+
+    if answer.strip() == "": answer = default
+
+    return answer if convert is None else convert(answer)
---
+++ PythonUtils/Ldap.py
@@ -0,0 +1,182 @@
+
+
+__all__ = ('LdapWrapper',)
+
+import ldap
+import ldap.modlist
+import json
+
+
+class LdapWrapper(object):
+    def __init__(self, host, ldaps=False):
+        port = 389
+        if ldaps:
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+            port = 636
+
+        url = '%s://%s:%d' % (('ldaps' if ldaps else 'ldap'), host, port)
+
+        self.__ldapobj = ldap.initialize(url)
+        self.__ldapobj.protocol_version = ldap.VERSION3
+
+        self.__bund = False
+
+
+    def bind(self, bind_dn, bind_pw):
+        self.__ldapobj.simple_bind_s(bind_dn, bind_pw)
+        self.__bund = True
+
+
+    def unbind(self):
+        if self.__bund:
+            self.__ldapobj.unbind_s()
+            self.__bund = False
+
+
+    def search(self, base_dn, search_scope=ldap.SCOPE_SUBTREE,
+               search_filter='(objectClass=*)', attrs=None, limit=0):
+        '''  do a ldapsearch, return all the matched entries in a list'''
+        rid = self.__ldapobj.search(base_dn, search_scope, search_filter, attrs)
+
+        count = 0
+        while True:
+            count += 1
+            if limit > 0 and count > limit:
+                return
+
+            r_type, r_data = self.__ldapobj.result(rid, 0)
+            if r_type == ldap.RES_SEARCH_ENTRY:
+                yield r_data[0]
+            else:
+                return
+
+
+    def add(self, add_dn, objectClass, **attrs):
+        if not self.__bund:
+            raise RuntimeError('Ldap is unbund, cannot add')
+
+        attrs['objectClass'] = objectClass
+        ldif = ldap.modlist.addModlist(attrs)
+        try:
+            self.__ldapobj.add_s(add_dn, ldif)
+        except Exception as e:
+            if __debug__: print e
+            pass
+
+
+    def delete(self, bind_dn, bind_pw, del_dn):
+        if not self.__bund:
+            raise RuntimeError('Ldap is unbund, cannot add')
+
+        try:
+            self.__ldapobj.delete_s(del_dn)
+        except Exception as e:
+            if __debug__: print e
+            pass
+
+
+    def modify(self, mod_dn, attr, oldv, newv):
+        if not self.__bund:
+            raise RuntimeError('Ldap is unbund, cannot add')
+
+        ldif = ldap.modlist.modifyModlist({attr : oldv}, {attr : newv})
+        try:
+            self.__ldapobj.modify_s(mod_dn, ldif)
+        except Exception as e:
+            if __debug__: print e
+            pass
+
+
+    def rename(self, dn, newdn):
+        if not self.__bund:
+            raise RuntimeError('Ldap is unbund, cannot add')
+
+        try:
+            self.__ldapobj.renames(dn, newdn)
+        except Exception as e:
+            if __debug__: print e
+            pass
+
+
+    def valid_user(self, base_dn, uid, user_pass, uid_field='uid'):
+        try:
+            search_filter='(%s=%s)' % (uid_field, uid)
+            print search_filter
+            entries = self.__ldapobj.search_s(base_dn, ldap.SCOPE_SUBTREE,
+                                            search_filter, [])
+
+            if len(entries) == 0:
+                print 'No such user'
+                return False
+
+
+            user_dn = entries[0][0]
+            bund = self.__bund
+
+            self.bind(user_dn, user_pass)
+
+            if __debug__:
+                print json.dumps(entries[0], indent=4)
+
+            if not bund:
+                self.unbind()
+
+            return True
+        except Exception as e:
+            if __debug__: print e
+            return False
+
+
+#
+# ------ main ------
+if __name__ == '__main__':
+    import sys
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option('--host', default='ldap-master.core.hp.com') # HPE
+    parser.add_option('--ldaps', action='store_true', default=False)
+    parser.add_option('-b', '--basedn', default='o=hp.com')
+    parser.add_option('--limit', type=int, default=100)
+    parser.add_option('--user')
+    parser.add_option('--password')
+
+    opts, args = parser.parse_args()
+    if opts.basedn is None:
+        print 'basedn cannot be empty'
+        sys.exit(1)
+
+    lw = LdapWrapper(opts.host, opts.ldaps)
+
+    #valid user
+    if opts.user and opts.password:
+        print lw.valid_user(opts.basedn, opts.user, opts.password,
+                           'hpUnixUserName')
+        sys.exit(0)
+
+#   #print all groups
+#   print 'Groups: (first 100)'
+#   base_dn = 'ou=Groups,' + opts.basedn
+#   for dn, entry in lw.search(base_dn, search_scope=ldap.SCOPE_ONELEVEL,
+#                              search_filter='(cn=*)',
+#                              limit=100):
+#       group = {'dn' : dn,
+#                'cn' : entry['cn'][0]}
+
+#       print group
+
+    #print all users
+    print 'Users: (first %d)' % opts.limit
+    base_dn = 'ou=People,' + opts.basedn
+    for dn, entry in lw.search(base_dn, search_scope=ldap.SCOPE_ONELEVEL,
+                               search_filter='(hpUnixUserName=*)',
+                               limit=opts.limit):
+        user = {'dn': dn, 'uid' : entry['uid'][0]}
+
+        if entry.get('hpUnixUserName', None):
+            user['user'] = entry['hpUnixUserName'][0]
+
+        print user
+
+
+
