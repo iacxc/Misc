ad2a85c3be25916f0f0c7a00abf62f0ebc63fdba
Chengxin Cai <chengxin.cai@gmail.com>
2016-12-03 09:47:03 +0800

    seperate env and site

--- UNLOAD/package/scripts/params.py
+++ UNLOAD/package/scripts/params.py
@@ -8,6 +8,6 @@ from resource_management.libraries.script import Script
 config = Script.get_config()
 
 base_dir = config['configurations']['unload-env']['base.dir']
-concurrency = config['configurations']['unload-env']['concurrency']
-throttle = config['configurations']['unload-env']['throttle']
-garbage_seconds = config['configurations']['unload-env']['garbage.seconds']
+concurrency = config['configurations']['unload-site']['concurrency']
+throttle = config['configurations']['unload-site']['throttle']
+garbage_seconds = config['configurations']['unload-site']['garbage.seconds']
--- UNLOAD/package/scripts/status_params.py
+++ UNLOAD/package/scripts/status_params.py
@@ -8,6 +8,3 @@ from resource_management.libraries.script import Script
 config = Script.get_config()
 
 base_dir = config['configurations']['unload-env']['base.dir']
-concurrency = config['configurations']['unload-env']['concurrency']
-throttle = config['configurations']['unload-env']['throttle']
-garbage_seconds = config['configurations']['unload-env']['garbage.seconds']
