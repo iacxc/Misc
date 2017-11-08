4495f3ffa09113d5a9d179657b58ebca752cc161
Cai Chengxin <chengxin.cai@gmail.com>
2017-11-08 08:56:21 +0800

    don't show user/password

--- EdwOpt/getdata.bat
+++ EdwOpt/getdata.bat
@@ -4,8 +4,8 @@ jython dumpData.py --classpath c:\caiche\lib\java\hpt4jdbc.jar ^
     --driver com.hp.jdbc.HPT4Driver ^
     --server zeo.houston.hp.com ^
     --dsn ZEO_EDW ^
-    --user cheng-xin.cai@hpe.com ^
-    --password Iam@hpe.com ^
+    --user %USER% ^
+    --password %PASS% ^
     --table se_delta ^
     --action se_delta ^
     --start "2016-11-01 00:00:00.000" ^
--- EdwOpt/getdata.sh
+++ EdwOpt/getdata.sh
@@ -4,8 +4,8 @@ jython dumpData.py --classpath /h/caiche/lib/java/hpt4jdbc.jar \
     --driver com.hp.jdbc.HPT4Driver \
     --server zeo.houston.hp.com \
     --dsn ZEO_EDW \
-    --user cheng-xin.cai@hpe.com \
-    --password Iam@hpe.com \
+    --user $USER \
+    --password $PASS \
     --table events \
     --action events \
     --start "2016-11-01 00:00:00.000" \
